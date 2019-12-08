ec5db0d31280638f6072bb14381e4a5ef51daa26
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2019-12-09 00:24:42 +0530

    Log-in and Sign up components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { LandComponent } from './land/land.component'
+import { LoginComponent } from './login/login.component'
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
+  { path: '', component: LandComponent },
   { path: 'home', component: HomeComponent },
   { path: 'department', component: DepartmentListComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', component: LandComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent }
 ];
 
 @NgModule( {
@@ -21,5 +26,7 @@ export const routingComponents = [
   HomeComponent,
   DepartmentListComponent,
   AboutComponent,
-  LandComponent
+  LandComponent,
+  LoginComponent,
+  SignUpComponent
 ]
